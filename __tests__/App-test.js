6f10c0a9ccbc8855e1e5c37e8e993e05d712d4b5
jasmaa <jasonmaa3955@gmail.com>
2019-07-10 18:25:28 -0400

    Fixing build

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,22 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-navigation', () => {
+    return {
+        createAppContainer: jest.fn().mockReturnValue(function NavigationContainer(props) {return null;}),
+        createDrawerNavigator: jest.fn(),
+        createMaterialTopTabNavigator: jest.fn(),
+        createStackNavigator: jest.fn(),
+        StackActions: {
+            push: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/PUSH"})),
+            replace: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/REPLACE"})),
+        },
+        NavigationActions: {
+            navigate: jest.fn().mockImplementation(x => x),
+        }
+    }
+});
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
